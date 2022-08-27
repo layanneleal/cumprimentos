55b6c73720b41370313e684d43c50b47a6e10651
Layanne Bezerra Leal <layanneliz@gmail.com>
2022-08-27 17:36:47 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 function verificar() {
   
-  let rel= document.querySelector('input#rel')
-  let res= document.querySelector('div#res')  
-  let agora= new date()
-  let hora= agora.getHours()
-  let relogio  = Number(rel.value)
- 
+  // capturando os componentes da página
+  let rel = document.querySelector('input#rel')
+  let res = document.querySelector('div#res')
 
-  if (hora < 12) {
-    res.innerHTML += `<p><strong>Bom dia!!</strong></p>`
+  // chamando a função DATE 
+  let agora = new Date()
 
-  }else if (hora <= 18) {
-    res.innerHTML += `<p><strong>Boa tarde !!</strong></p>`
+  // pegando somente hora do horário atual
+  let hora = agora.getHours()
+  let minuto = agora.getMinutes();
+  
+  // convertendo para numero a entrada de dados
+  let relogio = Number(rel.value)
+  
+  //console.log(relogio)
+  //console.log(agora)
+  //console.log(hora)
 
-  }else{
-    res.innerHTML += `<p><strong>Boa noite!!</strong></p>`
+  // se for antes do meio-dia
+  if (relogio <= 12) {
+
+    res.innerHTML = `<p><strong>Bom dia!!</strong></p>`
+
   }
-  res.innerHTML= `<p>São exatamente: ${hora}!</p>`
+  // se for entre meio-dia e final da tarde
+  else if (relogio > 12 && relogio <= 18) {
+
+    res.innerHTML = `<p><strong>Boa tarde !!</strong></p>`
+
+  }
+  // se for outro horário, noite
+  else{
+
+    res.innerHTML = `<p><strong>Boa noite!!</strong></p>`
+
+  }
+
+  res.innerHTML += `<p>Mas, o horário atual é este : ${hora}h${minuto} !</p>`
 }
